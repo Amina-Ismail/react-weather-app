faa0524688240a98b81e05e1444f420bb7195efd
Amina <amina0ismail0@gmail.com>
2023-10-23 14:40:58 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Amina Ismail and is{" "}
-        <a
-          href="https://github.com/Amina-Ismail/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+    <div className="App ">
+      <div class="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was created by Amina Ismail and is{" "}
+          <a
+            href="https://github.com/Amina-Ismail/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
