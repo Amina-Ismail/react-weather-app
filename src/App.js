3fbb60795cccafb8bb2089b4ccbdf4eace30d30b
Amina <amina0ismail0@gmail.com>
2023-10-23 12:25:02 +0200

    Added GitHub repository

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was created by Amina Ismail and is{" "}
+        <a
+          href="https://github.com/Amina-Ismail/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
