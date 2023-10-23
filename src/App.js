976b8a30da034dfd1beb6ea84eca88ef9eddd017
Amina <amina0ismail0@gmail.com>
2023-10-23 15:21:46 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
@@ -6,7 +7,7 @@ export default function App() {
     <div className="App ">
       <div class="container">
         <h1>Weather App</h1>
-
+        <Weather />
         <footer>
           This project was created by Amina Ismail and is{" "}
           <a
